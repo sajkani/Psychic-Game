934fb7b02442ce7df95abca306127e73fdc5fa90
Nicholas R Sajka <sajkani@OHBOCMBPNRS.local>
2017-05-08 22:03:59 -0400

    functional with exception of guesses so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,44 @@
-var computerChoices = [ "a", "b" , "c" ];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
-var guesses =0;
+var guessesLeft = 9;
+var guesses =9;
+var guessedLetters = [];
+var lettertoGuess = null;
 
 document.onkeyup = function(event) {
 
-	var userGuess = event.key;
+  var userGuess = event.key;
 
-	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-	if ( userGuess === computerGuess ){
-		wins++;
-	}
-	else if( userGuess != computerGuess ) {
-		losses++;
-	}
+  var reset = function () {
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
 
-	var html = "<p>Guess what letter I'm thinking of</p>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>";
+  }
+
+  if ( userGuess === computerGuess ){
+    wins++;
+    reset();
+  }
+  else if( userGuess != computerGuess ) {
+    guessesLeft--;
+  }
+
+  if ( guessesLeft === 0) {
+    losses++;
+    reset();
+  }
+
+  var html = "<p>Guess what letter I'm thinking of</p>" +
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>"+
+          "<p>Guesses Left:" + guessesLeft +"</p>"+
+          "<p>Your Guesses So Far:" + guessedLetters +"</p>";
 
     document.querySelector("#game").innerHTML = html;
 
